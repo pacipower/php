59fb759245bd39afef5a41ce33b0f850c494bd92
pacipower <pacipower7@gmail.com>
2019-12-12 19:57:46 +0100

    asd

--- index.php
+++ index.php
@@ -25,10 +25,11 @@ $statement ->execute();
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Könyveim</title>
+    <link rel="stylesheet" href="src/css/konyv.css">
 </head>
 <body>
 
-<table>
+<table id="mybooks">
     <tr>
         <th>
             Cím
